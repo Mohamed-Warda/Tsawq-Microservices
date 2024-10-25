e975b0d26d11ce7fd35d1d4c966c4208532b8cfe
Mohamed Warda <m.salman@aliaict.com>
2024-10-26 02:19:06 +0300

    Add CQRS Abstractions With MediatR

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+
+// overload that returns no value
+// 'Unit' is basically the void type for mediator
+public interface ICommand:ICommand<Unit>
+{
+}
+
+
+public interface ICommand<out TResponse>:IRequest<TResponse>
+{
+    
+}
