930dc4a9fe96fd68a0824120b69e7716e128d8be
Mohamed Warda <m.salman@aliaict.com>
2024-10-27 17:40:01 +0300

    Add Global Using File and First Catalog End Point

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct;
+
+
+//todo search record vs class
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+
+internal class CreateProductCommandHandler:ICommandHandler<CreateProductCommand,CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
