2dd2d4e3194c4147f81c28e527e75e2360046b69
Mohamed Warda <m.salman@aliaict.com>
2024-10-28 05:01:12 +0300

    Add Marten Library

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,30 @@
-﻿using BuildingBlocks.CQRS;
-
-namespace Catalog.API.Products.CreateProduct;
-
+﻿namespace Catalog.API.Products.CreateProduct;
 
 //todo search record vs class
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price)
     : ICommand<CreateProductResult>;
+
 public record CreateProductResult(Guid Id);
 
-
-internal class CreateProductCommandHandler:ICommandHandler<CreateProductCommand,CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var product = new Product();
+        product.Name = command.Name;
+        product.Description = command.Description;
+        product.Category = command.Category;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+        return new CreateProductResult(product.Id);
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(
+    new DependencyContextAssemblyCatalog(assemblies: typeof(Program).Assembly)//todo lect54 questions
+);
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("DataBase"));
+}).UseLightweightSessions();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapCarter();
 app.Run();
